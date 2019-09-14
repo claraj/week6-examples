6e39f95937e1d2c4b2566beca72169b21f65c975
clara <clara@minneapolis.edu>
2019-09-13 21:33:24 -0500

    fix copyright symbol

--- fetch_iss_and_map_updates_set_interval/fetch.js
+++ fetch_iss_and_map_updates_set_interval/fetch.js
@@ -9,7 +9,7 @@ var update = 10000  // 10 seconds
 
 var map = L.map('iss-map').setView([0, 0], 1)  // Center at 0, 0 and max zoom out
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 7,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiY2xhcmFsIiwiYSI6ImNqcmdwenViYTAwcHQ0Ym5yYmZ1Z3E2bjgifQ.QQfUvVaqPsWb_jJbP2gvHg'
