09410ed23e3416f42785acd18612f44e5ad3ce4a
clara <10088152+claraj@users.noreply.github.com>
2020-09-18 14:55:08 -0500

    display video

--- apod/apod.js
+++ apod/apod.js
@@ -1,25 +1,41 @@
 let apodApiUrl = 'https://apodapi.herokuapp.com/api/'
-
+ 
 let apodImage = document.querySelector('#apod-image')
+let apodVideo = document.querySelector('#apod-video')
 let apodDescription = document.querySelector('#apod-description')
 
 
 fetch(apodApiUrl)
     .then( response => response.json()).then( data => {
+
+        // Display the media. It could be an image or a video. 
         if (data.media_type === 'image') {
             let imgUrl = data.url
             apodImage.src = imgUrl
+            apodImage.style.display = 'block'
         }
         else if (data.media_type === 'video') {
-            // todo display video 
+            let videoUrl = data.url
+            apodVideo.src = videoUrl
+            apodVideo.style.display = 'block'
         }
         else {
             // What other types could there be? 
         }
 
+        // Display description - which may also be in the explanation property. 
         let description = data.description
-        apodDescription.innerHTML = description
+        let explanation = data.explanation
 
+        if (description) {
+            apodDescription.innerHTML = description
+        } 
+        else if (explanation) {
+            apodDescription.innerHTML = explanation
+        }
+        else {
+            apodDescription.innerHTML = 'No description available'
+        }
     })
     .catch( err => {
         console.log(err)
